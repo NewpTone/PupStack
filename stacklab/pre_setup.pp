98a75d4ceccf302444762c68087aa0c04a7c7dfe
yuxcer <yuxcer@126.com>
2012-11-16 12:07:42 +0800

    add xjtu region

--- stacklab/pre_setup.pp
+++ stacklab/pre_setup.pp
@@ -32,9 +32,41 @@ host {'ntp-server':
 }
 
 host {'stacklab-master':
-	name => 'sws-yz-10.master.com',
-	ip	 => '123.126.53.170',
+	comment			=> 'Stacklab master node',
+	name		    => 'sws-yz-10.master.com',
+	host_aliases	=> 'sws-yz-10',
+	ip				=> '123.126.53.170',
 }
+host {'xjtu-1':
+	name			=> 'xjtu-1.controller.edu',
+	host_aliases	=> 'xjtu-1',
+	ip				=> '123.139.159.3',
+	}
+host {'xjtu-2':
+	name			=> 'xjtu-2.compute.edu',
+	host_aliases	=> 'xjtu-2',
+	ip				=> '123.139.159.4',
+	}
+host {'xjtu-3':
+	name			=> 'xjtu-3.compute.edu',
+	host_aliases	=> 'xjtu-3',
+	ip				=> '123.139.159.5',
+	}
+host {'xjtu-4':
+	name			=> 'xjtu-4.compute.edu',
+	host_aliases	=> 'xjtu-4',
+	ip				=> '123.139.159.7',
+	}
+host {'xjtu-5':
+	name			=> 'xjtu-5.compute.edu',
+	host_aliases	=> 'xjtu-5',
+	ip				=> '123.139.159.13',
+	}
+host {'xjtu-6':
+	name			=> 'xjtu-6.compute.edu',
+	host_aliases	=> 'xjtu-6',
+	ip				=> '123.139.159.15',
+	}
 
 class {'ntp':
 	 servers    => [ 'ntp.sjtu.edu.cn' ],
